0e0f8753f98a2ece107686264daef8c76f703af6
Johnny Matos <128853698+DS-Johnny@users.noreply.github.com>
2024-07-22 15:06:34 -0300

    Treino Basico finalizado

--- Home.py
+++ Home.py
@@ -1,5 +1,38 @@
 import streamlit as st
 
-st.title("Hello World!")
-
 st.sidebar.title("Home")
+
+st.title("Aprenda Calistenia")
+
+st.write("Fonte do conteúdo: https://www.barzclub.net/")
+st.markdown("---")
+st.title("O que é Calistenia?")
+st.write("""Calistenia é o treino que utiliza apenas o peso do próprio corpo para fortalecer os músculos. Como por exemplo repetições de flexão, barra fixa e agachamentos.
+
+Pode ser feita em praticamente qualquer local, mas principalmente em praças, parques públicos e até em casa. E sua grande característica é que você pode treinar de graça e ter resultados incríveis.
+
+Essa modalidade de treino vem desde a antiguidade e tem sido por milênios a base do fortalecimento de exércitos, atletas e acrobatas.
+
+A palavra Calistenia vem da junção das palavras gregas kallos (beleza) e sthenos (força), sendo portanto o método de treino pra deixar o seu corpo belo, perfeito e ao mesmo tempo forte, como você deve perceber nos vídeos de Calistenia no Youtube.""")
+
+st.markdown("### Treinos:")
+
+basico, iniciante = st.tabs(['Treino Básico', 'Treino Iniciante'])
+
+with basico:
+
+    col1, col2 = st.columns(2)
+    with col1:
+        st.write("Treino de Flexão")
+        st.image("images/treino-flexao-calistenia-iniciante-basico.png")
+        st.markdown("---")
+        st.write("Treino de Core e Pernas")
+        st.image("images/treino-core-pernas-calistenia-iniciante-basico.png")
+
+    with col2:
+        st.write("Treino de Barra")
+        st.image("images/treino-barra-calistenia-iniciante-basico.png")
+        st.markdown("---")
+        st.write("Grade")
+        st.image("images/grade-treinos-calistenia-iniciante-basico.png")
+        
--- pages/2_Treino_Basico.py
+++ pages/2_Treino_Basico.py
@@ -2,23 +2,36 @@ import streamlit as st
 
 
 st.sidebar.title("Treino Básico")
+st.sidebar.image("images/grade-treinos-calistenia-iniciante-basico.png")
 st.title("Treino Básico")
 
 tab1, tab2, tab3 = st.tabs(["Treino de Flexão", "Treino de Barra",  "Core e pernas"])
 
 
 with tab1:
-    st.write("Treino de Flexão")
+    st.markdown("### Orientações")
+    st.write("""Começamos o nosso programa com um treino pra você aprender uma das bases da calistenia, a flexão de braço.
+Ele é composto por 5 exercícios que vão fortalecer seu peitoral, braços e ombros para que você possa fazer a flexão normal num futuro próximo.
+
+*IMPORTANTE*: Você deve fazer 3 séries de cada exercício com 2 minutos de descanso entre estas séries e só depois passar para o próximo exercício.
+
+Existem alguns cuidados que você deve tomar para garantir a eficiência dos exercícios e evitar lesões:
+- Durante as flexões mantenha sempre o tronco alinhado com as pernas, sem levantar nem abaixar demais a cintura;
+- Ao descer, sempre dobre os cotovelos para trás e nunca para os lados;
+- Faça o movimento completo, descendo até quase tocar o peitoral no chão e subindo até esticar os braços;
+- Nos exercícios com hold no nome você deve segurar aquela posição pelo tempo estipulado.""")
+    
+    st.markdown("---")
 
     col1, col2 = st.columns(2)
     with col1:
         st.write("Knee push up")
-        st.image("knee_push_up.png", width=240)
+        st.image("images/knee_push_up.png", width=240)
 
         
     
     with col2:
-        st.write("video")
+        st.write("Video")
         st.video("https://www.youtube.com/watch?v=jWxvty2KROs")
 
     st.markdown("---")
@@ -26,12 +39,12 @@ with tab1:
     col1, col2 = st.columns(2)
     with col1:
         st.write("Incline push up")
-        st.image("incline_push_up.png", width=240)
+        st.image("images/incline_push_up.png", width=240)
 
         
     
     with col2:
-        st.write("video")
+        st.write("Video")
         st.video("https://www.youtube.com/watch?v=3WUUeM07i_Q")
 
     st.markdown("---")
@@ -39,12 +52,12 @@ with tab1:
     col1, col2 = st.columns(2)
     with col1:
         st.write("Push up hold")
-        st.image("push_up_hold.png", width=240)
+        st.image("images/push_up_hold.png", width=240)
 
         
     
     with col2:
-        st.write("video")
+        st.write("Video")
         st.video("https://www.youtube.com/watch?v=16-WWEQNiK0")
 
     st.markdown("---")
@@ -52,12 +65,12 @@ with tab1:
     col1, col2 = st.columns(2)
     with col1:
         st.write("Decline push up hold")
-        st.image("decline_push_up_hold.png", width=240)
+        st.image("images/decline_push_up_hold.png", width=240)
 
         
     
     with col2:
-        st.write("video")
+        st.write("Video")
         st.video("https://www.youtube.com/watch?v=xloazKLlfnw")
 
     st.markdown("---")
@@ -65,18 +78,176 @@ with tab1:
     col1, col2 = st.columns(2)
     with col1:
         st.write("Dips hold")
-        st.image("dips_hold.png", width=240)
+        st.image("images/dips_hold.png", width=240)
 
         
     
     with col2:
-        st.write("video")
+        st.write("Video")
         st.video("https://www.youtube.com/watch?v=K3YE-tG6kPo&pp=ygUTaG93IHRvIGRvIGRpcHMgaG9sZA%3D%3D")
 
 
 
 with tab2:
-    st.write("Treino de Barra")
+    st.markdown("### Orientações")
+    st.write("""Este treino tem o objetivo de te preparar para outro exercício base da Calistenia, a barra fixa.
+
+Ele é composto por 6 exercícios que fortalecerão seus antebraços, bíceps e costas.
+
+*IMPORTANTE: Você deve fazer 3 séries de cada exercício com 2 minutos de descanso entre estas séries e só depois passar para o próximo exercício.
+
+Alguns cuidados que devem ser tomados são:
+- Quando estiver pendurado na barra não deixe o pescoço enterrado entre os ombros;
+- Nos exercícios pull up, a palmas das mãos ficam voltadas pra frente;
+- Nos exercícios chin up, as palmas das mãos ficam voltadas para você;
+- No negative chin up você deve subir com impulso ou ajuda e descer bem devagar;
+- Nas barras inclinadas mantenha o corpo alinhado durante todo o movimento, sem deixar a cintura abaixar;
+- Nos exercícios com hold no nome você deve segurar aquela posição pelo tempo estipulado.""")
+    
+    st.markdown("---")
+    
+    col1, col2 = st.columns(2)
+    with col1:
+        st.write("Negative chin up")
+        st.image("images/negative_chin_up.png", width=240)
+
+    with col2:
+        st.write("Video")
+        st.video("https://www.youtube.com/watch?v=mjNHoibfrMo")
+
+    st.markdown("---")
+
+    col1, col2 = st.columns(2)
+    with col1:
+        st.write("Chin up hold")
+        st.image("images/chin_up_hold.png", width=240)
+
+    with col2:
+        st.write("Video")
+        st.video("https://youtu.be/tkLrUnNZ0SY?si=7vSGXSIQncDNHpK-")
+
+    st.markdown("---")
+
+    col1, col2 = st.columns(2)
+    with col1:
+        st.write("Scapula pull up")
+        st.image("images/scapula_pull_up.png", width=240)
+
+    with col2:
+        st.write("Video")
+        st.video("https://www.youtube.com/watch?v=VpeeqGkiDUU")
+
+    st.markdown("---")
+
+    col1, col2 = st.columns(2)
+    with col1:
+        st.write("Pull up hold")
+        st.image("images/pull_up_hold.png", width=240)
+
+    with col2:
+        st.write("Video")
+        st.video("https://www.youtube.com/watch?v=XBsvbpdNiEU&pp=ygUWaG93IHRvIGRvIHB1bGwgdXAgaG9sZA%3D%3D")
+
+
+    st.markdown("---")
+
+    col1, col2 = st.columns(2)
+    with col1:
+        st.write("Australian pull up")
+        st.image("images/australian_pull_up.png", width=240)
+
+    with col2:
+        st.write("Video")
+        st.video("https://www.youtube.com/watch?v=bHO0A4ZF_Zg")
+
+    st.markdown("---")
+
+    col1, col2 = st.columns(2)
+    with col1:
+        st.write("Incline chin up")
+        st.image("images/incline_chin_up.png", width=240)
+
+    with col2:
+        st.write("Video")
+        st.video("https://www.youtube.com/watch?v=Aaxq4ikl8PE")
+
 
 with tab3:
-    st.write("Core e pernas")
+    st.markdown("### Orientações")
+    st.write("""Ainda que o grande objetivo deste programa seja que você aprenda barra e flexão, não podemos negligenciar as outras partes do corpo.
+
+Por isso temos este treino de pernas, abdômen e lombar, grupos musculares importantíssimos até mesmo para se desenvolver nas barras, flexões e exercícios mais avançados.
+
+*IMPORTANTE: Você deve fazer 3 séries de cada exercício com 2 minutos de descanso entre estas séries e só depois passar para o próximo exercício.
+
+Para que o treino seja eficiente e seguro, tome os seguintes cuidados:
+- No agachamento mantenha o tronco alinhado e desça até as pernas dobrarem num ângulo um pouco menor que que 90°;
+- No hamstring levers, use a força das pernas para levantar o tronco;
+- No calf raises desça o calcanhar abaixo do nível da ponta dos pés e suba até a panturrilha contrair ao máximo;
+- No leg raises e legs hold, tente manter as pernas sempre esticadas;
+- No plank mantenha o corpo alinhado, sem levantar nem abaixar demais a cintura.""")
+
+    st.markdown("---")
+    col1, col2 = st.columns(2)
+    with col1:
+        st.write("Squat")
+        st.image("images/squat.png", width=240)
+    
+    with col2:
+        st.write("Video")
+        st.video("https://www.youtube.com/watch?v=xqvCmoLULNY")
+
+    st.markdown("---")
+
+    col1, col2 = st.columns(2)
+    with col1:
+        st.write("Hamstring levers")
+        st.image("images/hamstring_levers.png", width=240)
+
+    with col2:
+        st.write("Video")
+        st.video("https://www.youtube.com/watch?v=lbV2P9HvY7A&pp=ygUfaG93IHRvIGRvIGhhbXN0cmluZyBzaG9ydCBsZXZlcg%3D%3D")
+    
+    st.markdown("---")
+
+    col1, col2 = st.columns(2)
+    with col1:
+        st.write("Calf raises")
+        st.image("images/calf_raises.png", width=240)
+    
+    with col2:
+        st.write("Video")
+        st.video("https://www.youtube.com/watch?v=c5Kv6-fnTj8&pp=ygUVaG93IHRvIGRvIGNhbGYgcmFpc2Vz")
+
+    st.markdown("---")
+
+    col1, col2 = st.columns(2)
+    with col1:
+        st.write("Lying leg raises")
+        st.image("images/lying_leg_raises.png", width=240)
+
+    with col2:
+        st.write("Video")
+        st.video("https://www.youtube.com/watch?v=Wp4BlxcFTkE")
+
+    st.markdown("---")
+
+    col1, col2 = st.columns(2)
+    with col1:
+        st.write("Legs hold")
+        st.image("images/legs_hold.png", width=240)
+
+    with col2:
+        st.write("Video")
+        st.video("https://www.youtube.com/watch?v=fqzMd87uNTI")
+
+    st.markdown("---")
+
+    col1, col2 = st.columns(2)
+    with col1:
+        st.write("Plank")
+        st.image("images/plank.png", width=240)
+
+    with col2:
+        st.write("Video")
+        st.video("https://www.youtube.com/watch?v=q4rDeHYMcIg")
